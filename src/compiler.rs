0fb570d6a096b2b683235a8f26391cf0ab9b7eb4
听寒 <10476912+hycinth22@users.noreply.github.com>
2025-09-24 16:42:42 +0800

    compiler: fix if & for loop

--- src/compiler.rs
+++ src/compiler.rs
@@ -272,7 +272,7 @@ impl Compiler {
                 self.emit_opcode_with_arg(bytecode, OpCode::Jmp, 0);
 
                 // 填充 JZ 的偏移量
-                let jz_offset = bytecode.len() - jz_pos - OPLEN;
+                let jz_offset = bytecode.len() - jz_pos;
                 self.set_arg_at(bytecode, jz_pos, jz_offset as u64);
                 // 写入else分支
                 if let Some(else_block) = else_branch {
@@ -280,7 +280,7 @@ impl Compiler {
                 }
 
                 // 填充 JMP 的偏移量
-                let jmp_offset = bytecode.len() - jmp_pos - OPLEN;
+                let jmp_offset = bytecode.len() - jmp_pos;
                 self.set_arg_at(bytecode, jmp_pos, jmp_offset as u64);
             },
             ASTNode::ForLoop(init, condition, update, body) => {
@@ -320,11 +320,11 @@ impl Compiler {
                 }
 
                 // 跳回循环开始
-                let jmp_offset = loop_start as i64 - bytecode.len() as i64 - OPLEN as i64;
+                let jmp_offset = loop_start as i64 - bytecode.len() as i64;
                 self.emit_opcode_with_arg(bytecode, OpCode::Jmp, jmp_offset as u64);
 
                 // 填充 JZ 的偏移量
-                let jz_offset = bytecode.len() - jz_pos - OPLEN;
+                let jz_offset = bytecode.len() - jz_pos;
                 self.set_arg_at(bytecode, jz_pos, jz_offset as u64);
             },
             ASTNode::WhileLoop(condition, body) => {
